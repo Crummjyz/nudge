38daabf7001ee4363f766ac74496eced0624d8d6
Finn Eger <finn@fantail.dev>
2022-11-21 17:13:26 +1300

    Add function to find changed lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use regex::Regex;
+use std::{env, path::Path, process::Command};
+
+fn diff(path: &Path) -> Vec<usize> {
+    let diff = Command::new("git")
+        .arg("diff")
+        .arg("@~..@")
+        .arg("--unified=0")
+        .arg(path)
+        .current_dir(path.parent().unwrap())
+        .output()
+        .unwrap();
+    let output = String::from_utf8(diff.stdout).unwrap();
+
+    let regex = Regex::new(r"(?m)^@@ \-\d+(?:,\d+)* \+(\d+)(?:,\d+)* @@").unwrap();
+    let captures = regex.captures_iter(&output);
+    return captures
+        .map(|capture| capture[1].parse::<usize>().unwrap())
+        .collect();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new(&env::args().nth(1).unwrap()).to_owned();
+    let lines = diff(&path);
+    println!("{lines:?}")
 }
