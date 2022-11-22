958cd60195d7ec579d91d86a3424b7c9c69b8696
Finn Eger <finn@fantail.dev>
2022-11-22 15:37:32 +1300

    Improve errors

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,15 @@ use std::{
 };
 use tree_sitter::{Parser, Point, Tree};
 
+macro_rules! warn {
+    ($message:expr) => {
+        println!("::warning::{}", $message);
+    };
+    ($message:expr, $file:expr, $line:expr) => {
+        println!("::warning file={},line={}::{}", $file, $line, $message);
+    };
+}
+
 const KINDS: [&str; 7] = [
     "variable_declaration",
     "function_declaration",
@@ -26,15 +35,19 @@ fn diff(path: &Path) -> HashSet<usize> {
         .arg("@~..@")
         .arg("--unified=0")
         .arg(path)
-        .current_dir(path.parent().unwrap())
+        .current_dir(path.parent().expect("path should have a parent"))
         .output()
-        .unwrap();
-    let output = String::from_utf8(diff.stdout).unwrap();
+        .expect("failed to execute git diff");
+    let output = String::from_utf8(diff.stdout).expect("stdout should be utf-8");
 
     let regex = Regex::new(r"(?m)^@@ \-\d+(?:,\d+)* \+(\d+)(?:,\d+)* @@").unwrap();
     let captures = regex.captures_iter(&output);
     return captures
-        .map(|capture| capture[1].parse::<usize>().unwrap())
+        .map(|capture| {
+            capture[1]
+                .parse::<usize>()
+                .expect("capture should be a line number")
+        })
         .collect();
 }
 
@@ -45,12 +58,14 @@ fn comments(tree: &Tree, point: Point) -> Vec<Point> {
         let node = cursor.node();
         if KINDS.contains(&node.kind()) {
             // TODO: Check for start/end of multiline comments.
-            let comment = cursor.node().prev_named_sibling().unwrap();
-            if comment.kind() == "comment" {
+            if let Some(comment) = cursor
+                .node()
+                .prev_named_sibling()
+                .filter(|sibling| sibling.kind() == "comment")
+            {
                 comments.push(comment.start_position());
             }
         }
-
         cursor.goto_first_child_for_point(point);
         if cursor.node() == node {
             break;
@@ -62,13 +77,16 @@ fn comments(tree: &Tree, point: Point) -> Vec<Point> {
 fn find(path: &Path) {
     let lines = diff(path);
 
-    let mut file = File::open(path).unwrap();
+    let mut file = File::open(path).expect("file should exist");
     let mut source = String::new();
-    file.read_to_string(&mut source).unwrap();
+    file.read_to_string(&mut source)
+        .expect("file source should be readable");
 
     let mut parser = Parser::new();
     parser.set_language(tree_sitter_swift::language()).unwrap();
-    let tree = parser.parse(&source, None).unwrap();
+    let tree = parser
+        .parse(&source, None)
+        .expect("source code should parse");
 
     let comments: HashSet<Point> = lines
         .iter()
@@ -77,28 +95,33 @@ fn find(path: &Path) {
 
     let comments_lines: HashSet<usize> = comments.iter().map(|point| point.row).collect();
     for line in comments_lines.difference(&lines) {
-        println!(
-            "::warning file={},line={line}::Posssibly outdated comment",
-            path.display(),
-        );
+        warn!("Possible stale comment", path.display(), line);
     }
 }
 
 fn find_recursively(path: &Path) {
     if path.is_dir() {
-        for entry in fs::read_dir(path).unwrap() {
-            let path = entry.unwrap().path();
+        for entry in fs::read_dir(path).expect("path should be a readable dir") {
+            let entry = entry.unwrap();
+            let path = entry.path();
             find_recursively(&path);
         }
-    } else if path.extension() == Some(OsStr::new("swift")) {
-        find(&path)
+    } else {
+        if path.extension() == Some(OsStr::new("swift")) {
+            find(&path)
+        }
     }
 }
 
 fn main() {
     let paths: Vec<PathBuf> = env::args()
         .skip(1)
-        .map(|arg| Path::new(&arg).to_owned())
+        .map(|arg| {
+            Path::new(&arg)
+                .canonicalize()
+                .expect("arg should be a valid path")
+                .to_owned()
+        })
         .collect();
     for path in paths {
         find_recursively(&path);
