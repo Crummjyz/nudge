0990356f1e7a1c318c0eac1af245a0fd6751e141
Finn Eger <finn@fantail.dev>
2023-10-09 09:54:13 +1300

    Group comments into blocks

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/alexflint/go-arg"
 	"github.com/go-git/go-git/v5"
+	sitter "github.com/smacker/go-tree-sitter"
 
 	"fantail.dev/nudge/v2/internal"
 )
@@ -49,14 +50,60 @@ func main() {
 		content, changes := internal.Collect(patch.Chunks())
 		comments := internal.Sit(content, changes)
 
-		for _, comment := range comments {
+		blocks := []struct {
+			start, end sitter.Point
+			content    string
+		}{}
+
+		blocks = append(blocks, struct {
+			start, end sitter.Point
+			content    string
+		}{
+			start:   comments[0].StartPoint(),
+			end:     comments[0].EndPoint(),
+			content: comments[0].Content(content) + "\n",
+		})
+
+		for _, comment := range comments[1:] {
+			block := &blocks[len(blocks)-1]
+
+			if comment.StartPoint().Row == block.end.Row+1 {
+				block.end = comment.EndPoint()
+				block.content += comment.Content(content) + "\n"
+			} else {
+				blocks = append(blocks, struct {
+					start, end sitter.Point
+					content    string
+				}{
+					start:   comment.StartPoint(),
+					end:     comment.EndPoint(),
+					content: comment.Content(content),
+				})
+			}
+		}
+
+		for _, block := range blocks {
 			_, file := patch.Files()
 			path := file.Path()
 			switch args.WarningFormat {
 			case WarningFormat(DefaultWarningFormat):
-				fmt.Printf("%s:%d:%d: comment unchanged: %s\n", path, comment.StartPoint().Row+1, comment.StartPoint().Column+1, comment.Content(content))
+				fmt.Printf(
+					"%s:%d:%d: comment unchanged: \n%s\n",
+					path,
+					block.start.Row+1,
+					block.start.Column+1,
+					block.content,
+				)
 			case WarningFormat(GitHubWarningFormat):
-				fmt.Printf("::warning file=%s,line=%d,col=%d::%s\n", path, comment.StartPoint().Row+1, comment.StartPoint().Column+1, "comment unchanged")
+				fmt.Printf(
+					"::warning file=%s,line=%d,col=%d,endLine=%d,endCol=%d::%s\n",
+					path,
+					block.start.Row+1,
+					block.start.Column+1,
+					block.end.Row+1,
+					block.end.Column+1,
+					"comment unchanged",
+				)
 			}
 		}
 	}
