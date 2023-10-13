52febdef64fe7457cbf49d6bd924b829fbf5ba44
Finn Eger <finn@fantail.dev>
2023-10-13 15:24:22 +1300

    Fix find algorithm

--- internal/find/find.go
+++ internal/find/find.go
@@ -30,27 +30,70 @@ func FindComments(file []byte, changes [][2]int) ([]*sitter.Node, error) {
 }
 
 func recurse(node *sitter.Node, changes [][2]int) (comments []*sitter.Node) {
+	pending := []*sitter.Node{}
+
+	ended := false   // the comment has ended
+	changed := false // the comment has changed
+
 	for i := 0; i < int(node.NamedChildCount()); i++ {
 		child := node.NamedChild(i)
 
-		subchanges := [][2]int{}
-		for _, change := range changes {
-			if overlap(change, [2]int{int(child.StartByte()), int(child.EndByte())}) {
-				subchanges = append(subchanges, change)
-			}
-		}
+		overlap := overlappingChanges(child, changes)
 
-		if len(subchanges) > 0 {
-			comments = append(comments, recurse(child, subchanges)...)
-		} else if child.Type() == "line_comment" {
-			comments = append(comments, child)
+		switch child.Type() {
+		case "line_comment":
+			if ended {
+				pending = nil
+				ended = false
+			}
+
+			if len(overlap) > 0 {
+				changed = true
+				continue
+			}
+
+			pending = append(pending, child)
+		default:
+			ended = true
+
+			if changed {
+				pending = nil
+				changed = false
+			}
+
+			if len(overlap) > 0 {
+				comments = append(comments, pending...)
+				pending = nil
+
+				comments = append(comments, recurse(child, overlap)...)
+			}
+
+			// Language Specific Rules
+
+			// We only want to match direct siblings, but property wrappers and attributes
+			// complicate this, so we special case them. This may lead to some suboptimal code paths
+			// in the above logic.
+
+			if child.Type() != "attribute_item" {
+				break
+			}
+
+			pending = nil
 		}
 	}
+	return
+}
 
+func overlappingChanges(node *sitter.Node, changes [][2]int) (overlapping [][2]int) {
+	for _, change := range changes {
+		if overlap(change, [2]int{int(node.StartByte()), int(node.EndByte())}) {
+			overlapping = append(overlapping, change)
+		}
+	}
 	return
 }
 
 // overlap reports whether the (ends-inclusive) ranges a and b overlap at all.
 func overlap(a, b [2]int) bool {
-	return a[0] <= b[0] && b[0] <= a[1] || a[0] <= b[1] && b[1] <= a[1]
+	return (b[0] >= a[0] && b[0] <= a[1]) || (b[1] >= a[0] && b[1] <= a[1]) || (a[0] >= b[0] && a[0] <= b[1]) || (a[1] >= b[0] && a[1] <= b[1])
 }
